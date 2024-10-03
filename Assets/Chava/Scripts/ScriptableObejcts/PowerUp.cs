40f5048061eb69c2d9f8e4a1f698f4dc4b918b87
Chavadex <salvador.cruz@uniat.edu.mx>
2024-10-02 19:25:42 -0600

    Cambios menores

--- Assets/Chava/Scripts/ScriptableObejcts/PowerUp.cs
+++ Assets/Chava/Scripts/ScriptableObejcts/PowerUp.cs
@@ -5,14 +5,17 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "NewPowerUp", menuName = "PowerUp")]
 public class PowerUp : ScriptableObject
 {
-    public float swimSpeedModifier = 2.0f;  // Modificador de velocidad de nado
-    public float walkSpeedModifier = 2.0f;  // Modificador de velocidad de caminar
-    public float duration = 5.0f;           // Esto es cuanto dura el power-up en segundos
-    public Sprite powerUpSprite;            // Sprite del power-up
+    public float swimSpeedModifier = 2.0f;  
+    public float walkSpeedModifier = 2.0f; 
+    public float duration = 5.0f;           
+    public Sprite powerUpSprite;            
 
     
     public void ApplyPowerUp(PlayerMovement player)
     {
         player.ModifySpeed(swimSpeedModifier, walkSpeedModifier, duration);
     }
+
+
+
 }
